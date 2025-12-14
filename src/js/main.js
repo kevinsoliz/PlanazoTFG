d22437729c06ae0bdd2ee64ffe9d64e2b62c75b4
Kevin Soliz <kevinsoliz10@gmail.com>
2025-12-14 14:31:44 +0100

    La versión con js mantiene el hover

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
+collapsibles.forEach((elemento) =>
+  elemento.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
+
+    let navIcon = this.querySelector('.nav__toggler use');
+    console.log(navIcon);
+    if(navIcon) {
+      let icono = navIcon.getAttribute('href');
+
+      if(icono.includes('menu'))
+        navIcon.setAttribute('href', 'images/iconos.svg#close');
+      
+      else
+        navIcon.setAttribute('href', 'images/iconos.svg#menu');
+    }
   })
 );
