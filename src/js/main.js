4097445489b261d99f7a6f43b07c887aebb1bae8
Kevin Soliz <kevinsoliz10@gmail.com>
2025-12-15 16:32:48 +0100

    Component collapsible modificado, ahora solo se expande cuando pulso en el icono

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,35 @@
 
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((elemento) =>
-  elemento.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
+// const collapsibles = document.querySelectorAll(".collapsible");
 
-    let navIcon = this.querySelector('.nav__toggler use');
+// console.log(collapsibles);
+
+// collapsibles.forEach((elemento) => elemento.addEventListener("click", function () {
+
+//     this.classList.toggle("collapsible--expanded");
+
+
+//     let navIcon = this.querySelector('.nav__toggler use');
+//     console.log(navIcon);
+//     if(navIcon) {
+//       let icono = navIcon.getAttribute('href');
+
+//       if(icono.includes('menu'))
+//         navIcon.setAttribute('href', 'images/iconos.svg#close');
+      
+//       else
+//         navIcon.setAttribute('href', 'images/iconos.svg#menu');
+//     }
+//   })
+// );
+
+const icons = document.querySelectorAll(".collapsible .icon--collapser");
+
+icons.forEach((icono) => icono.addEventListener("click", function(event) {
+
+    const collapsibleDiv = icono.closest(".collapsible");
+    collapsibleDiv.classList.toggle("collapsible--expanded");
+
+     let navIcon = this.querySelector('.nav__toggler use');
     console.log(navIcon);
     if(navIcon) {
       let icono = navIcon.getAttribute('href');
@@ -15,5 +40,6 @@ collapsibles.forEach((elemento) =>
       else
         navIcon.setAttribute('href', 'images/iconos.svg#menu');
     }
+
   })
 );
