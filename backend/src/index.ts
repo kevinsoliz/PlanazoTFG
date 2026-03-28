13eb25f22df1f590d51765db5f77d3ee7c5ccec1
k-soliz <k.soliz@moyca.eu>
2026-03-28 11:49:40 +0100

    configurar sesiones de Express con PostgreSQL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,49 @@
 import express from "express";
 import cors from "cors";
+import session from "express-session";
+import connectPgSimple from "connect-pg-simple";
 import pool from "./db";
-                                                                    
-  const app = express();
-  const PORT = process.env.PORT || 4000;
 
-  app.use(cors({
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const PGStore = connectPgSimple(session);
+
+app.use(
+  cors({
     origin: "http://localhost:3000",
     credentials: true, //permite que las peticiones incluyan cookies
-  }));
-
+  }),
+);
 
 //json es un metodo estático de express que es una función y objeto a la vez.
-  app.use(express.json()); //convierte los json a objetos
+app.use(express.json()); //convierte los json a objetos
 
-  app.get("/api/health", (req, res) => { // ruta para comprobar el estado del servidor
-    res.json({ status: "ok" });
-  });
+app.use(
+  session({
+    store: new PGStore({
+      pool,
+      createTableIfMissing: true,
+    }),
+    secret: process.env.SESSION_SECRET || "secreto_desarrollo",
+    name: "session_id",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    },
+  }),
+);
 
-  app.listen(PORT, () => {
-    console.log(`Servidor backend corriendo en
+app.get("/api/health", (req, res) => {
+  // ruta para comprobar el estado del servidor
+  res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor backend corriendo en
   http://localhost:${PORT}`);
-  });
+});
