dbb8b94a38cea216fdd78a312c3dee2d1d30c0f4
k-soliz <k.soliz@moyca.eu>
2026-03-28 09:10:43 +0100

    agrega punto de entrada del backend con express

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+                                                                    
+  const app = express();
+  const PORT = process.env.PORT || 4000;
+
+  app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true, //permite que las peticiones incluyan cookies
+  }));
+
+
+//json es un metodo estático de express que es una función y objeto a la vez.
+  app.use(express.json()); //convierte los json a objetos
+
+  app.get("/api/health", (req, res) => { // ruta para comprobar el estado del servidor
+    res.json({ status: "ok" });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Servidor backend corriendo en
+  http://localhost:${PORT}`);
+  });
