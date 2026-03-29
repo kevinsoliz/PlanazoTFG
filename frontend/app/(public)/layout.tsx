52d31f66aea997d0c6dda9a21711382703765114
Kevin Soliz <kevinsoliz10@gmail.com>
2026-03-29 12:58:35 +0200

    primera iteracion diseno de registro

--- frontend/app/(public)/layout.tsx
+++ frontend/app/(public)/layout.tsx
@@ -7,8 +7,9 @@ const PublicLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => 
         <div className="flex-1 flex items-center justify-center bg-base-100 p-8">
             {children}
         </div>
-        <div className="hidden lg-flex flex-1 bg-primary items-center justify-center">
-            
+        <div className="hidden lg:flex flex-1 bg-primary items-center justify-center">
+          <p className="text-2xl">hola mundo</p>
+            <img src="src/images/auth/amigos12.png" alt="" />
         </div>
     </div>
     </>
