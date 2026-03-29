f87a971466032558870707f8e8e83efbaad108b4
k-soliz <k.soliz@moyca.eu>
2026-03-29 12:35:23 +0200

    reorganiza imagenes

--- frontend/app/components/landing/Oruga.tsx
+++ frontend/app/components/landing/Oruga.tsx
@@ -4,12 +4,12 @@ const Oruga = () => {
   return (
     <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
         <div className="flex -space-x-3">
-            <img src="/images/profiles/person1.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
-            <img src="/images/profiles/person2.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
-            <img src="/images/profiles/person3.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
-            <img src="/images/profiles/person4.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
-            <img src="/images/profiles/person5.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
-            <img src="/images/profiles/person6.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
+            <img src="/images/landing/oruga-avatars/person1.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
+            <img src="/images/landing/oruga-avatars/person2.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
+            <img src="/images/landing/oruga-avatars/person3.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
+            <img src="/images/landing/oruga-avatars/person4.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
+            <img src="/images/landing/oruga-avatars/person5.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
+            <img src="/images/landing/oruga-avatars/person6.png" className="w-10 h-10 rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.4)]" />
         </div>
         <p><b className='text-teal-500'>+2000</b> personas ya se han unido!</p>
     </div>
