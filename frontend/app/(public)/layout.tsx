22537cf9d87978f81e34a6e4ed044f69a05edeb6
Kevin Soliz <kevinsoliz10@gmail.com>
2026-03-30 13:08:10 +0200

    diseno del registro casi listo, hay que separar el logo en un componente aislado

--- frontend/app/(public)/layout.tsx
+++ frontend/app/(public)/layout.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const PublicLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+const PublicLayout = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
   return (
     <>
-    <div className="min-h-screen flex">
-        <div className="flex-1 flex items-center justify-center bg-base-100">
+      <div className="min-h-screen flex">
+        <div className="flex-1 flex flex-col p-8">
+          <p className="text-2xl">LOGO</p>
+          <div className="flex-1 flex items-center justify-center">
             {children}
+          </div>
         </div>
-        <div className="hidden lg:flex flex-1/4 bg-primary items-center justify-center">
-        
-            <img src="/images/auth/amigos12.png" className="w-3/5" alt="" />
-        </div>
-    </div>
-    </>
-  )
-}
 
-export default PublicLayout
+        <div className="hidden lg:flex flex-1/4 bg-primary items-center justify-center">
+          <img src="/images/auth/amigos12.png" className="w-3/5" alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PublicLayout;
--- frontend/app/(public)/registro/page.tsx
+++ frontend/app/(public)/registro/page.tsx
@@ -1,19 +1,29 @@
 const Registro = () => {
   return (
-    <fieldset className="fieldset w-3/5">
-      <div className="pb-10">
+    <>
+      <fieldset className="fieldset w-3/5">
+        <div className="pb-10">
           <legend className="text-4xl pb-2">Bienvenido</legend>
           <p>Registrate para empezar</p>
-      </div>
+        </div>
 
-      <label className="label">Email</label>
-      <input type="email" className="input w-full" placeholder="tu@email.com" />
+        <label className="label">Email</label>
+        <input
+          type="email"
+          className="input w-full"
+          placeholder="tu@email.com"
+        />
 
-      <label className="label">Password</label>
-      <input type="password" className="input w-full" placeholder="••••••••" />
+        <label className="label">Password</label>
+        <input
+          type="password"
+          className="input w-full"
+          placeholder="••••••••"
+        />
 
-      <button className="btn btn-neutral mt-4">Login</button>
-    </fieldset>
+        <button className="btn btn-neutral mt-4">Login</button>
+      </fieldset>
+    </>
   );
 };
 
