f7007b6556a7a5dcd91394a4ab1140b7d737e5a5
Kevin Soliz <kevinsoliz10@gmail.com>
2026-03-30 17:11:01 +0200

    agrega logica de registro con fetch y sesiones

--- frontend/app/(public)/registro/page.tsx
+++ frontend/app/(public)/registro/page.tsx
@@ -1,14 +1,65 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
 const Registro = () => {
+  const [newUser, setNewUser] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleRegistro = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:4000/api/auth/registro", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify(newUser),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError(data.error);
+        return;
+      }
+
+      router.push("/planes");
+    } catch {
+      setError("Error de conexión con el servidor");
+    }
+  };
+
   return (
-    <>
-      <fieldset className="fieldset w-3/5">
+    <form onSubmit={handleRegistro} className="w-3/5">
+      <fieldset className="fieldset">
         <div className="pb-10">
           <legend className="text-4xl pb-2">Bienvenido</legend>
           <p>Registrate para empezar</p>
         </div>
+        <label className="label">
+          <input
+            onChange={(event) =>
+              setNewUser({ ...newUser, nombre: event.target.value })
+            }
+            type="text"
+            className="input w-full"
+            placeholder="Tu nombre"
+          />
+        </label>
 
         <label className="label">Email</label>
         <input
+          onChange={(event) =>
+            setNewUser({ ...newUser, email: event.target.value })
+          }
           type="email"
           className="input w-full"
           placeholder="tu@email.com"
@@ -16,14 +67,18 @@ const Registro = () => {
 
         <label className="label">Password</label>
         <input
+          onChange={(event) =>
+            setNewUser({ ...newUser, password: event.target.value })
+          }
           type="password"
           className="input w-full"
           placeholder="••••••••"
         />
 
-        <button className="btn btn-neutral mt-4">Login</button>
+        {error && <p className="text-error text-center">{error}</p>}
+        <button className="btn btn-neutral mt-4">Registrarse</button>
       </fieldset>
-    </>
+    </form>
   );
 };
 
