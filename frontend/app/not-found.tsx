2d513e63946ceabad41b90383360a37359827b6c
Kevin Soliz <kevinsoliz10@gmail.com>
2026-03-30 20:22:10 +0200

    agrega vistas para error 404 y 500 y tambien ruta test-error para probar el 500

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import BaseCard from "./components/ui/BaseCard";
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+        <div className="relative">
+            <img src="/images/status-code/amigo-sentado.png" className="hidden lg:block absolute bottom-20 -left-44  w-80 z-10" alt="" />
+            <BaseCard>
+              <div className="card-body items-center text-center">
+                <h1 className="text-4xl font-bold">Página no encontrada</h1>
+                <p>La página que buscas no existe o fue movida</p>
+                <Link href="/" className="btn btn-primary mt-4">
+                  Volver al inicio
+                </Link>
+              </div>
+            </BaseCard>
+        </div>
+    </div>
+  );
+};
+
+export default NotFound;
