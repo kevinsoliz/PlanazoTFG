9b7a10c7cd13bb3a938bb660602363fd2af60f57
Kevin Soliz <kevinsoliz10@gmail.com>
2026-03-30 23:07:31 +0200

    el deploy esta costando, tenia la url del backend hardcodeada, crea fichero env

--- frontend/app/(public)/login/page.tsx
+++ frontend/app/(public)/login/page.tsx
@@ -17,7 +17,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:4000/api/auth/login", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
