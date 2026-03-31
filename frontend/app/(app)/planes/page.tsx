1486a1438e8a0d40d2eba3beba40a60993a353b6
Kevin Soliz <kevinsoliz10@gmail.com>
2026-03-31 22:44:42 +0200

    agrega paquete react-icons y avanza un poco con el diseno de la app

--- frontend/app/(app)/planes/page.tsx
+++ frontend/app/(app)/planes/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { FaBell} from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
 
 const Planes = () => {
   return (
-    <div>Planes</div>
-  )
-}
+    <section>
+      <div className="flex justify-between p-5 gap-5">
+        
+          <label className="input rounded-full">
+            <CiSearch className="text-neutral/40" />
+            <input type="text" placeholder="Buscar" />
+          </label>
+          
+        
+        <div className="flex items-center gap-6">
+          <FaBell size={20} className="text-primary cursor-pointer"/>
+          <FaMessage size={20} className="text-primary cursor-pointer"/>
+          <button className="btn btn-primary btn-outline">Crear plan</button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Planes
+export default Planes;
