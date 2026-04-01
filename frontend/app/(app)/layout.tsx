6f7e42ae42f08b415ddf793cef1da857803ba40c
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-01 20:35:00 +0200

    disenando el sidebar donde ira el perfil del usuario en desktop, he encontrado un bug, esta en la issue #39"

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "../components/ui/Logo";
+import Sidebar from "../components/layout/Sidebar";
 
 const AppLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div className="min-h-screen flex p-4">
-      <div className=" w-80">
+      <div className=" flex flex-col w-65">
         <Logo />
+        <Sidebar/>
       </div>
       <div className="flex-2 bg-base-100 rounded-xl border-2">
         {children}
