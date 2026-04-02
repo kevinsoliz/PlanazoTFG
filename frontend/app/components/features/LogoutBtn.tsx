6b65a38551f171a967c6e2a0e8d2159f28fd3572
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-02 19:13:29 +0200

    agrega componente logout que redirecciona afuera

--- frontend/app/components/features/LogoutBtn.tsx
+++ frontend/app/components/features/LogoutBtn.tsx
@@ -1,9 +1,32 @@
-'use client'
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FiLogOut } from "react-icons/fi";
 
 const LogoutBtn = () => {
-  return (
-    <div>LogoutBtn</div>
-  )
-}
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
+        method: "POST",
+        credentials: "include",
+      });
 
-export default LogoutBtn
+      //pase lo que pase hay que mandar al usuario a fuera
+      router.push("/");
+
+    } catch {
+      router.push("/");
+    }
+  };
+  return (
+    <>
+      <FiLogOut size={22} />
+      <button className="font-bold hover:cursor-pointer" onClick={handleLogout}>
+        Logout
+      </button>
+    </>
+  );
+};
+
+export default LogoutBtn;
