cc737ab2fafac89d9ce79c8c1460fa76dc8d10ab
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-02 17:12:56 +0200

    masonry en marcha

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -4,12 +4,12 @@ import Sidebar from "../components/layout/Sidebar";
 
 const AppLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <div className="min-h-screen flex p-4">
+    <div className="flex p-4">
       <div className=" flex flex-col w-75">
         <Logo />
         <Sidebar/>
       </div>
-      <div className="flex-2 bg-amber-600/15 rounded-4xl ">
+      <div className="flex-2 flex bg-amber-600/15 rounded-4xl">
         {children}
       </div>
     </div>
