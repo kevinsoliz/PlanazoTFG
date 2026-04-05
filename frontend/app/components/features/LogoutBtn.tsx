22c16d0f8048049da06d687bfc6b112ef77eb814
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-05 21:31:41 +0200

    refactoriza logoutBtn con el authService

--- frontend/app/components/features/LogoutBtn.tsx
+++ frontend/app/components/features/LogoutBtn.tsx
@@ -1,23 +1,16 @@
 "use client";
 
+import authService from "@/app/services/auth-service";
 import { useRouter } from "next/navigation";
 import { FiLogOut } from "react-icons/fi";
 
 const LogoutBtn = () => {
   const router = useRouter();
-  const handleLogout = async () => {
-    try {
-      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
-        method: "POST",
-        credentials: "include",
-      });
-
-      //pase lo que pase hay que mandar al usuario a fuera
-      router.push("/");
-
-    } catch {
-      router.push("/");
-    }
+  const handleLogout = () => {
+    authService
+      .logout()
+      .then(() => router.push("/"))
+      .catch(() => router.push("/"));
   };
   return (
     <>
