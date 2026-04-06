8697a8a164084d0c3787d32591ed84ffc99f86f3
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-06 11:59:00 +0200

    agrega coalesce a la ruta del patch de perfiles para que el username se quede como esta si no se cambia

--- backend/src/routes/perfiles.ts
+++ backend/src/routes/perfiles.ts
@@ -4,48 +4,50 @@ import { requireAuth } from "../middleware/auth";
 
 const router = Router();
 
-router.get("/perfiles/:id", async(req, res) => {
+router.get("/perfiles/:id", async (req, res) => {
+  try {
+    let perfil = await pool.query("SELECT * FROM perfiles WHERE user_id = $1", [
+      req.params.id,
+    ]);
 
-    try {
-
-        let perfil = await pool.query("SELECT * FROM perfiles WHERE user_id = $1", [req.params.id])
-        
-        if (perfil.rows.length === 0) {
-            res.status(404).json( { error: "Perfil no encontrado "});
-            return;
-        }
-    
-        res.json( { perfil: perfil.rows[0] });
-
-    } catch (error) {
-        res.status(500).json( { error: "Error del servidor" });
+    if (perfil.rows.length === 0) {
+      res.status(404).json({ error: "Perfil no encontrado " });
+      return;
     }
 
-})
+    res.json({ perfil: perfil.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: "Error del servidor" });
+  }
+});
 
 router.patch("/perfiles/:id", requireAuth, async (req, res) => {
-    if (Number(req.params.id) != req.session.userId) {
-        res.status(403).json( { error: "No puedes editar el perfil de otra persona."});
-        return;
+  if (Number(req.params.id) != req.session.userId) {
+    res
+      .status(403)
+      .json({ error: "No puedes editar el perfil de otra persona." });
+    return;
+  }
+
+  const { nombre, username, descripcion, categorias } = req.body;
+
+  try {
+    const resultado = await pool.query(
+      `UPDATE perfiles 
+            SET nombre = $1, username = COALESCE($2, username), descripcion = $3, categorias = $4 
+            WHERE user_id = $5 
+            RETURNING *`,
+      [nombre, username, descripcion, categorias, req.params.id],
+    );
+
+    if (resultado.rows.length === 0) {
+      res.status(404).json({ error: "Perfil no encontrado." });
+      return;
     }
 
-    const { nombre, username, descripcion, categorias } = req.body;
-
-    try {
-        const resultado = await pool.query("UPDATE perfiles SET nombre = $1, username = $2, descripcion = $3, categorias = $4 WHERE user_id = $5 RETURNING *", 
-            [nombre, username, descripcion, categorias, req.params.id]
-        );
-
-        if (resultado.rows.length === 0) {
-            res.status(404).json({ error: "Perfil no encontrado."});
-            return;
-        }
-        
-        res.json({ perfil: resultado.rows[0] });
-
-    } catch (error) {
-        res.status(500).json({ error: "Error del servidor."})
-    }
-
-} )
+    res.json({ perfil: resultado.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: "Error del servidor." });
+  }
+});
 export default router;
