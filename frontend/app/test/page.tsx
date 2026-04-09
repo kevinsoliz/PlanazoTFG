40010fc93a2ee7095ef68699227adbb4f639c5ba
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-09 22:41:18 +0200

    agrega servicio fetch de perfiles para el servidor next.js

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -1,9 +1,9 @@
 import { fetchServer } from '@/app/lib/api-server'
-import { getPlanes } from '../services/planes.server';
+import { getPerfil } from '../services/auth.server';
 
 export default async function TestPage() {
-    const data = await getPlanes();
-    console.log("Data recibiiiiiii:", data);
+    const data = await getPerfil();
+    console.log("Perfil en la data:", data);
 
     return <pre>{JSON.stringify(data, null, 2)}</pre>
 }
