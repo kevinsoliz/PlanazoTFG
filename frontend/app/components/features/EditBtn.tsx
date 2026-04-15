9a89bc26840bc5ee33e0d1234d7581ccaa3560ed
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-15 21:21:01 +0200

    usa useRef para persistir el dialog

--- frontend/app/components/features/EditBtn.tsx
+++ frontend/app/components/features/EditBtn.tsx
@@ -1,18 +1,36 @@
-'use client'
+"use client";
 
 import { Plan } from "@/app/types/plan";
+import { useRef, useState } from "react";
 
-const EditBtn = ({ plan }: {plan: Plan}) => {
+const EditBtn = ({ plan }: { plan: Plan }) => {
+  const [currentPlan, setCurrentPlan] = useState({
+    titulo: plan.titulo,
+    categoria: plan.categoria,
+    descripcion: plan.descripcion,
+    fecha: plan.fecha,
+    ubicacion: plan.ubicacion,
+    aforo_max: plan.aforo_max,
+  });
+
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const handleClick = () => {
+
+    dialogRef.current?.showModal();
+
+    console.log("plan actual:", plan);
+  };
   return (
     <>
-      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+      {/* (You can open the modal using document.getElementById('ID').showModal() method) -> de DaisyUI, para no tener que usar el document usamos useRef que persiste la referencia al elemento html dialog*/}
       <button
         className="btn btn-success btn-sm btn-outline"
-        onClick={() => (document.getElementById("my_modal_3") as HTMLDialogElement).showModal()}
+        onClick={handleClick}
       >
         Editar
       </button>
-      <dialog id="my_modal_3" className="modal">
+      <dialog ref={dialogRef} className="modal">
         <div className="modal-box">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
@@ -20,11 +38,10 @@ const EditBtn = ({ plan }: {plan: Plan}) => {
               ✕
             </button>
           </form>
-          <h3 className="font-bold text-lg">{plan.titulo}</h3>
-          <p className="py-4">{plan.descripcion}</p>
-          <p className="py-4">{plan.fecha}</p>
-          <p className="py-4">{plan.categoria}</p>
-          
+          <h3 className="font-bold text-lg">{currentPlan.titulo}</h3>
+          <p className="py-4">{currentPlan.descripcion}</p>
+          <p className="py-4">{currentPlan.fecha}</p>
+          <p className="py-4">{currentPlan.categoria}</p>
         </div>
       </dialog>
     </>
