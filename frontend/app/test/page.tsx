1b5b6291efb2b8a5de05f2eb3d8abbdaea4a38e2
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-21 22:36:17 +0200

    empieza a iterar sobre el form base

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -1,21 +1,10 @@
 'use client'
 import { editarPlan } from '../actions/planes';
-import { editarPerfil } from '../services/auth.server';
+import FormBase from '../components/features/FormBase';
+
 
 export default function TestPage() {
 
-    const handleEditar = async () => {
-        debugger;
-        const resultado = await editarPerfil({
-            nombre: "Pericoooo",
-            username: "juuuan",
-            descripcion: "HOLA QUE ASE",
-            categorias: "Aventura, Música, Cine"
-        })
 
-        console.log(resultado)
-
-    }
-
-    return <button onClick={handleEditar}>Editar</button>
+    return <FormBase/>
 }
