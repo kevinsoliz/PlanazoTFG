33e524a37e51652a9a812b3e3e2397dde8c80f76
Kevin Soliz <kevinsoliz10@gmail.com>
2026-04-25 16:55:27 +0200

    agrega module.css para la estructura

--- frontend/app/(app)/home/page.tsx
+++ frontend/app/(app)/home/page.tsx
@@ -1,7 +1,8 @@
+import AnularBtn from "@/app/components/features/AnularBtn";
 import JoinBtn from "@/app/components/features/JoinBtn";
 import PlanCard from "@/app/components/features/PlanCard";
 import { getPlanes } from "@/app/services/planes.server";
-
+import styles from "./page.module.css"
 
 // server component, los datos se obtienen antes de renderizar, el servidor de next.js renderiza el html antes de enviarlo al navegador
 
@@ -9,15 +10,424 @@ const Planes = async () => {
   const planes = await getPlanes();
 
   return (
-    <section className=" flex-1 flex flex-col overflow-y-auto rounded-xl pt-4">
-      <article className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-6 ">
-        {planes.map((plan) => (
-          <PlanCard key={plan.id} plan={plan}>
-            <JoinBtn plan_id={plan.id}/>
-          </PlanCard>
-        ))}
-      </article>
-    </section>
+    <div className={`${styles.home}`}>
+     
+       
+          <article className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-3 ">
+            {planes.map((plan) => (
+              <PlanCard key={plan.id} plan={plan}>
+                <JoinBtn plan_id={plan.id} />
+              </PlanCard>
+            ))}
+          </article>
+     
+
+      {/* Esto es un div invisible que ocupa el mismo ancho que el aside */}
+      {/* <div className="hidden lg:block w-200 shrink-0"></div> */}
+      <aside className={`${styles.stack} hidden lg:block`}>
+        <ul className="list bg-base-100 rounded-box shadow-md">
+          <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
+            Most played songs this week
+          </li>
+
+          <li className="list-row">
+            <div>
+              <img
+                className="size-10 rounded-box"
+                src="https://img.daisyui.com/images/profile/demo/1@94.webp"
+              />
+            </div>
+            <div>
+              <div>Dio Lupa</div>
+              <div className="text-xs uppercase font-semibold opacity-60">
+                Remaining Reason
+              </div>
+            </div>
+            <p className="list-col-wrap text-xs">
+              "Remaining Reason" became an instant hit, praised for its haunting
+              sound and emotional depth. A viral performance brought it
+              widespread recognition, making it one of Dio Lupa’s most iconic
+              tracks.
+            </p>
+            
+          <button className="btn btn-warning btn-xs">Anular</button>
+          </li>
+
+          <li className="list-row">
+            <div>
+              <img
+                className="size-10 rounded-box"
+                src="https://img.daisyui.com/images/profile/demo/4@94.webp"
+              />
+            </div>
+            <div>
+              <div>Ellie Beilish</div>
+              <div className="text-xs uppercase font-semibold opacity-60">
+                Bears of a fever
+              </div>
+            </div>
+            <p className="list-col-wrap text-xs">
+              "Bears of a Fever" captivated audiences with its intense energy
+              and mysterious lyrics. Its popularity skyrocketed after fans
+              shared it widely online, earning Ellie critical acclaim.
+            </p>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M6 3L20 12 6 21 6 3z"></path>
+                </g>
+              </svg>
+            </button>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                </g>
+              </svg>
+            </button>
+          </li>
+
+          <li className="list-row">
+            <div>
+              <img
+                className="size-10 rounded-box"
+                src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+              />
+            </div>
+            <div>
+              <div>Sabrino Gardener</div>
+              <div className="text-xs uppercase font-semibold opacity-60">
+                Cappuccino
+              </div>
+            </div>
+            <p className="list-col-wrap text-xs">
+              "Cappuccino" quickly gained attention for its smooth melody and
+              relatable themes. The song’s success propelled Sabrino into the
+              spotlight, solidifying their status as a rising star.
+            </p>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M6 3L20 12 6 21 6 3z"></path>
+                </g>
+              </svg>
+            </button>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                </g>
+              </svg>
+            </button>
+          </li>
+          <li className="list-row">
+            <div>
+              <img
+                className="size-10 rounded-box"
+                src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+              />
+            </div>
+            <div>
+              <div>Sabrino Gardener</div>
+              <div className="text-xs uppercase font-semibold opacity-60">
+                Cappuccino
+              </div>
+            </div>
+            <p className="list-col-wrap text-xs">
+              "Cappuccino" quickly gained attention for its smooth melody and
+              relatable themes. The song’s success propelled Sabrino into the
+              spotlight, solidifying their status as a rising star.
+            </p>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M6 3L20 12 6 21 6 3z"></path>
+                </g>
+              </svg>
+            </button>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                </g>
+              </svg>
+            </button>
+          </li>
+          <li className="list-row">
+            <div>
+              <img
+                className="size-10 rounded-box"
+                src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+              />
+            </div>
+            <div>
+              <div>Sabrino Gardener</div>
+              <div className="text-xs uppercase font-semibold opacity-60">
+                Cappuccino
+              </div>
+            </div>
+            <p className="list-col-wrap text-xs">
+              "Cappuccino" quickly gained attention for its smooth melody and
+              relatable themes. The song’s success propelled Sabrino into the
+              spotlight, solidifying their status as a rising star.
+            </p>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M6 3L20 12 6 21 6 3z"></path>
+                </g>
+              </svg>
+            </button>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                </g>
+              </svg>
+            </button>
+          </li>
+          <li className="list-row">
+            <div>
+              <img
+                className="size-10 rounded-box"
+                src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+              />
+            </div>
+            <div>
+              <div>Sabrino Gardener</div>
+              <div className="text-xs uppercase font-semibold opacity-60">
+                Cappuccino
+              </div>
+            </div>
+            <p className="list-col-wrap text-xs">
+              "Cappuccino" quickly gained attention for its smooth melody and
+              relatable themes. The song’s success propelled Sabrino into the
+              spotlight, solidifying their status as a rising star.
+            </p>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M6 3L20 12 6 21 6 3z"></path>
+                </g>
+              </svg>
+            </button>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                </g>
+              </svg>
+            </button>
+          </li>
+          <li className="list-row">
+            <div>
+              <img
+                className="size-10 rounded-box"
+                src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+              />
+            </div>
+            <div>
+              <div>Sabrino Gardener</div>
+              <div className="text-xs uppercase font-semibold opacity-60">
+                Cappuccino
+              </div>
+            </div>
+            <p className="list-col-wrap text-xs">
+              "Cappuccino" quickly gained attention for its smooth melody and
+              relatable themes. The song’s success propelled Sabrino into the
+              spotlight, solidifying their status as a rising star.
+            </p>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M6 3L20 12 6 21 6 3z"></path>
+                </g>
+              </svg>
+            </button>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                </g>
+              </svg>
+            </button>
+          </li>
+          <li className="list-row">
+            <div>
+              <img
+                className="size-10 rounded-box"
+                src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+              />
+            </div>
+            <div>
+              <div>Sabrino Gardener</div>
+              <div className="text-xs uppercase font-semibold opacity-60">
+                Cappuccino
+              </div>
+            </div>
+            <p className="list-col-wrap text-xs">
+              "Cappuccino" quickly gained attention for its smooth melody and
+              relatable themes. The song’s success propelled Sabrino into the
+              spotlight, solidifying their status as a rising star.
+            </p>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M6 3L20 12 6 21 6 3z"></path>
+                </g>
+              </svg>
+            </button>
+            <button className="btn btn-square btn-ghost">
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
+                </g>
+              </svg>
+            </button>
+          </li>
+        </ul>
+      </aside>
+    </div>
   );
 };
 
