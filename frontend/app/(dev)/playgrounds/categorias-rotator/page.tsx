e057eadbfb7c16570c179b851a8cbf7ba225d860
kevinsoliz <kevinsoliz10@gmail.com>
2026-04-30 17:25:51 +0200

    agrega categoriaas animacion

---
+++ frontend/app/(dev)/playgrounds/categorias-rotator/page.tsx
@@ -0,0 +1,11 @@
+import CategoriasRotator from "@/app/components/ui/CategoriasRotator";
+
+const CategoriasRotatorPlayground = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <CategoriasRotator />
+    </div>
+  );
+};
+
+export default CategoriasRotatorPlayground;
