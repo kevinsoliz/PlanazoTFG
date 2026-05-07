fe1d5c8b220967bbd9d53f80a14b1e5996249240
SergioReq <1851937@alu.murciaeduca.es>
2026-05-07 23:43:13 +0200

    canWrite true por defecto en el botón modal

--- frontend/app/components/features/planes/ChatModalBtn.tsx
+++ frontend/app/components/features/planes/ChatModalBtn.tsx
@@ -33,7 +33,7 @@ export default function ChatModalBtn({ planId, userName, userId, planTitulo }: {
               >✕</button>
             </div>
             <div className="flex-1 flex flex-col bg-base-100 overflow-hidden">
-              <ChatPlan planId={planId} userName={userName} userId={userId} />
+              <ChatPlan planId={planId} userName={userName} userId={userId} canWrite={true} /> {/* En este modal, el usuario siempre puede escribir, ya que para abrirlo ya se ha comprobado que es participante. Si quieres ser más estricto, podrías pasar canWrite como prop desde el padre, pero en este caso lo dejamos siempre true. */}
             </div>
           </div>
           <div className="modal-backdrop" onClick={() => setIsOpen(false)}></div>
